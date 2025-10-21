99d3c0dcdbab6e08ae52db3ca69207b31cd215ed
parishasahu <parishajbp2007@gmail.com>
2025-10-21 13:19:26 +0530

    Remove obsolete practice scripts and add new QR code generator and colorful text projects

--- practice/list_ps1.py
+++
@@ -1,6 +0,0 @@
-my_list = []
-for i in range(1,8):
-    name=input(f"{i}.enter name of the fruit: ")
-    my_list.append(name)
-
-print(my_list)
--- practice/list_ps4.py
+++
@@ -1,7 +0,0 @@
-my_list = (7,0,8,0,0,9)
-
-print(my_list.count(0))
-s=set()
-set.add(5)
-set.add(8,6,9)
-print()
